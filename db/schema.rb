1b73a04eed5fab4030a9840f4bf0b35f76540ca7
Miguel Cortes <cortes.miguel@msn.com>
2015-07-17 11:56:45 -0400

    added column to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716191825) do
+ActiveRecord::Schema.define(version: 20150717153953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,10 @@ ActiveRecord::Schema.define(version: 20150716191825) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "creator_id"
   end
+
+  add_index "games", ["creator_id"], name: "index_games_on_creator_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
